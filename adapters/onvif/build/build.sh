03813f9a00f13b29154d2c8c923a84b145b903a4
Tianyou Li <tianyou.li@gmail.com>
2015-11-08 06:52:52 +0000

    remove adapters directory

--- adapters/onvif/build/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-CUR_DIR=`pwd`
-BLD_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-cd ${BLD_DIR}
-cmake -G"Eclipse CDT4 - Unix Makefiles" -D_ECLIPSE_VERSION=4.3 -DCMAKE_CXX_COMPILER_ARG1=-std=c++0x ${BLD_DIR}
-make
-cd ${CUR_DIR}
--- adapters/onvif/build/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CUR_DIR=`pwd`
-BLD_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-
-cd ${BLD_DIR}
-rm -rf CMakeCache.txt CMakeFiles Makefile cmake_install.cmake
-cd ${CUR_DIR}
-
