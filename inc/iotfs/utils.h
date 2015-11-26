e9f3cc08adc2e45314d923fc84b0e5e66b4d51d3
Tianyou Li <tianyou.li@gmail.com>
2015-11-26 07:08:01 +0000

    remove code, refactor code

--- inc/iotfs/utils.h
+++ inc/iotfs/utils.h
@@ -4,23 +4,25 @@
 #include <sstream>
 #include <string>
 #include <boost/lexical_cast.hpp>
+#include <boost/algorithm/string.hpp>
 
 /**
  * specialized version for bool lexical_cast<bool>("true")
  */
 namespace boost {
 template <> bool lexical_cast<bool, std::string>(const std::string &arg) {
-  std::istringstream ss(arg);
-  bool b;
-  ss >> std::boolalpha >> b;
-  return b;
+
+  if (boost::iequals(arg, std::string("true"))) {
+    return true;
+  }
+
+  if (boost::iequals(arg, std::string("false"))) {
+    return false;
+  }
+  
+  throw bad_lexical_cast();  
 }
 
-template <> std::string lexical_cast<std::string, bool>(const bool &b) {
-  std::ostringstream ss;
-  ss << std::boolalpha << b;
-  return ss.str();
-}
 }
 
 #endif
