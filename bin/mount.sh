d88eb75f4e45ed9d61c2b3e259f78bcc3dd22599
Tianyou Li <tianyou.li@gmail.com>
2015-11-30 10:43:14 +0000

    add mount.sh helper script

---
+++ bin/mount.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+CUR_DIR=`pwd`
+BLD_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+export IOTFS_BIN_DIR=${BLD_DIR}
+
+cd ${BLD_DIR}
+mkdir -p temp
+./iotfs ./temp
+cd ${CUR_DIR}
