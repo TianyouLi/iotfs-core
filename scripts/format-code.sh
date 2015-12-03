1f7ebc0316926e9b8cf9b2b1ff9491066596e244
Tianyou Li <tianyou.li@gmail.com>
2015-12-03 01:48:38 +0000

    add license header

--- scripts/format-code.sh
+++ scripts/format-code.sh
@@ -7,9 +7,20 @@ cd ${BLD_DIR}
 STYLE=${BLD_DIR}/.clang-format
 FORMAT="clang-format -i "
 
-echo "find ${BLD_DIR}/../src ${BLD_DIR}/../inc -iname "*.cpp" -o -iname "*.h" | xargs ${FORMAT}"
-find ${BLD_DIR}/../src ${BLD_DIR}/../inc -iname "*.cpp" -o -iname "*.h" | xargs ${FORMAT}
+FILES=`find ${BLD_DIR}/../src ${BLD_DIR}/../inc -iname "*.cpp" -o -iname "*.h"`
+LICENSE=${BLD_DIR}/../LICENSE
 
+for f in ${FILES}
+do
+	if ! grep -q Copyright ${f}
+	then
+		echo "Add license header to ${f}"
+		(echo "/*"; cat ${LICENSE}; echo "*/"; echo; cat ${f}) >${f}.new && mv ${f}.new ${f}
+	fi
+
+	echo "Formatting code for ${f}"
+	${FORMAT} ${f}
+done
 
 
 cd ${CUR_DIR}
